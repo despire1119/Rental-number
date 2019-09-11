bd4917cb8184ced912f127de078090909848240b
王皞然 <wanghaoran0218@hotmail.com>
2019-09-11 17:11:44 +0800

    init

---
+++ src/views/Contact.vue
@@ -0,0 +1,116 @@
+<template>
+  <div class="games">
+    <div class="container">
+      <div class="page-path">
+        <ul class="path-list">
+          <li>
+            <a href="index.html">Home</a>
+          </li>&nbsp;&nbsp;/&nbsp;&nbsp;
+          <li class="act">Contact</li>
+        </ul>
+        <div class="clearfix" />
+      </div>
+      <div class="contact">
+        <h3 class="page-header">Cotntact Us</h3>
+        <div id="container" class="map">
+          <!-- <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d12071.681938475434!2d-93.25993179999999!3d40.851669550000004!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x87e9175fdc81fa0f%3A0x3e508b796d85b60e!2sNew+York%2C+IA+50238%2C+USA!5e0!3m2!1sen!2sin!4v1418810233989"  frameborder="0" style="border:0"></iframe> -->
+        </div>
+        <div class="contact-box">
+          <div class="text">
+            <input type="text" placeholder="Name" required>
+            <input type="text" placeholder="Subject" required>
+            <input type="text" placeholder="Email" required>
+          </div>
+          <div class="text">
+            <textarea placeholder="Message" required />
+          </div>
+          <div class="text-but">
+            <input type="submit" value="submit">
+          </div>
+        </div>
+        <div class="address-box">
+          <h3 class="page-header">Address</h3>
+          <h4>It is a long established fact that a reader will be distracted</h4>
+          <p>
+            Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content
+            here, content here',
+          </p>
+          <p>
+            Lorem Ipsum is that it has a more-or-less normal distribution of letters,that it has a more-or-less normal
+            distribution as opposed to using 'Content here, content here',
+          </p>
+          <a href="mailto:example@email.com">123example@email.com</a>
+          <p>+001 111 9999 99</p>
+        </div>
+        <div class="clearfix" />
+      </div>
+    </div>
+  </div>
+</template>
+<style lang="stylus" scoped>
+.map iframe
+  width 100%
+  height 250px
+.map
+  margin 1em 0
+.contact-box
+  width 70%
+  float left
+  margin-right 3%
+.address-box
+  width 27%
+  float left
+.text
+  display flex
+  justify-content space-between
+  margin 1em 0
+  input[type='text']
+    width 28%
+    padding 10px
+    border 1px solid #ff7105
+    outline none
+    -webkit-appearance none
+  textarea
+    width 97.7%
+    height 250px
+    border 1px solid #ff7105
+    outline none
+    -webkit-appearance none
+    padding 10px
+    resize none
+.text-but
+  margin 1em 0
+  text-align center
+.text-but input[type='submit'], .text-cmt input[type='submit']
+  font-size 0.875em
+  color #fff
+  text-transform uppercase
+  background #ff7105
+  padding 10px 20px
+  outline none
+  -webkit-appearance none
+  cursor pointer
+  border 0
+.text-but input[type='submit']:hover, .text-cmt input[type='submit']:hover
+  background #4fbfa8
+  transition 0.5s all
+  -webkit-transition 0.5s all
+  -o-transition 0.5s all
+  -moz-transition 0.5s all
+  -ms-transition 0.5s all
+.address-box h4
+  font-size 0.875em
+  color #000
+  font-weight 500
+  text-transform uppercase
+  margin-bottom 0.5em
+.address-box p
+  font-size 0.8em
+  font-weight 100
+  line-height 1.6em
+  margin 0.5em 0
+.address-box a
+  font-size 0.8em
+  color #4fbfa8
+  font-weight 100
+</style>
---
+++ src/views/Games.vue
@@ -0,0 +1,333 @@
+<template>
+  <div>
+    <div class="games">
+      <div class="container">
+        <div class="page-path">
+          <ul class="path-list">
+            <li>
+              <a href="javascript:;">Home</a>
+            </li>&nbsp;&nbsp;/&nbsp;&nbsp;
+            <li class="act">Games</li>
+          </ul>
+          <p>
+            <a href="javascript:;">Back to Home</a>
+          </p>
+          <div class="clearfix" />
+        </div>
+        <h3 class="page-header">List of Games Here</h3>
+        <section class="games-contain">
+          <aside class="side-bar">
+            <h4>New games</h4>
+            <ul class="game-list">
+              <li>
+                <a href="javascript:;">Shooting</a>
+              </li>
+              <li>
+                <a href="javascript:;">Action</a>
+              </li>
+              <li>
+                <a href="javascript:;">Cars</a>
+              </li>
+              <li>
+                <a href="javascript:;">Wressling</a>
+              </li>
+              <li>
+                <a href="javascript:;">bikes</a>
+              </li>
+              <li>
+                <a href="javascript:;">bicycle</a>
+              </li>
+              <li>
+                <a href="javascript:;">Racing</a>
+              </li>
+              <li>
+                <a href="javascript:;">Zombies</a>
+              </li>
+            </ul>
+            <h4>Old games</h4>
+            <ul class="game-list">
+              <li>
+                <a href="javascript:;">Shooting</a>
+              </li>
+              <li>
+                <a href="javascript:;">Action</a>
+              </li>
+              <li>
+                <a href="javascript:;">Cars</a>
+              </li>
+              <li>
+                <a href="javascript:;">Wressling</a>
+              </li>
+              <li>
+                <a href="javascript:;">bikes</a>
+              </li>
+              <li>
+                <a href="javascript:;">bicycle</a>
+              </li>
+              <li>
+                <a href="javascript:;">Racing</a>
+              </li>
+              <li>
+                <a href="javascript:;">Zombies</a>
+              </li>
+              <li>
+                <a href="javascript:;">Action</a>
+              </li>
+              <li>
+                <a href="javascript:;">Cars</a>
+              </li>
+              <li>
+                <a href="javascript:;">Wressling</a>
+              </li>
+            </ul>
+          </aside>
+          <div class="main">
+            <div class="view view-first">
+              <img :src="require('@/assets/images/1.jpg')">
+              <div class="mask">
+                <h2>Game Never</h2>
+                <p>A wonderful serenity has taken possession of my entire soul, like these sweet mornings of spring which I enjoy with my whole heart.</p>
+                <a href="javascript:;" class="info">Play It</a>
+              </div>
+            </div>
+            <div class="view view-first">
+              <img :src="require('@/assets/images/2.jpg')">
+              <div class="mask">
+                <h2>Online Game</h2>
+                <p>A wonderful serenity has taken possession of my entire soul, like these sweet mornings of spring which I enjoy with my whole heart.</p>
+                <a href="javascript:;" class="info">Play It</a>
+              </div>
+            </div>
+            <div class="view view-first">
+              <img :src="require('@/assets/images/3.jpg')">
+              <div class="mask">
+                <h2>Delmen lorem</h2>
+                <p>A wonderful serenity has taken possession of my entire soul, like these sweet mornings of spring which I enjoy with my whole heart.</p>
+                <a href="javascript:;" class="info">Play It</a>
+              </div>
+            </div>
+            <div class="view view-first">
+              <img :src="require('@/assets/images/4.jpg')">
+              <div class="mask">
+                <h2>Ipsum street</h2>
+                <p>A wonderful serenity has taken possession of my entire soul, like these sweet mornings of spring which I enjoy with my whole heart.</p>
+                <a href="javascript:;" class="info">Play It</a>
+              </div>
+            </div>
+            <div class="view view-first">
+              <img :src="require('@/assets/images/5.jpg')">
+              <div class="mask">
+                <h2>Games online</h2>
+                <p>A wonderful serenity has taken possession of my entire soul, like these sweet mornings of spring which I enjoy with my whole heart.</p>
+                <a href="javascript:;" class="info">Play It</a>
+              </div>
+            </div>
+            <div class="view view-first">
+              <img :src="require('@/assets/images/6.jpg')">
+              <div class="mask">
+                <h2>Nothing but Game</h2>
+                <p>A wonderful serenity has taken possession of my entire soul, like these sweet mornings of spring which I enjoy with my whole heart.</p>
+                <a href="javascript:;" class="info">Play It</a>
+              </div>
+            </div>
+            <div class="view view-first">
+              <img :src="require('@/assets/images/2.jpg')">
+              <div class="mask">
+                <h2>Ipsum street</h2>
+                <p>A wonderful serenity has taken possession of my entire soul, like these sweet mornings of spring which I enjoy with my whole heart.</p>
+                <a href="javascript:;" class="info">Play It</a>
+              </div>
+            </div>
+            <div class="view view-first">
+              <img :src="require('@/assets/images/1.jpg')">
+              <div class="mask">
+                <h2>Games online</h2>
+                <p>A wonderful serenity has taken possession of my entire soul, like these sweet mornings of spring which I enjoy with my whole heart.</p>
+                <a href="javascript:;" class="info">Play It</a>
+              </div>
+            </div>
+            <div class="view view-first">
+              <img :src="require('@/assets/images/4.jpg')">
+              <div class="mask">
+                <h2>Nothing but Game</h2>
+                <p>A wonderful serenity has taken possession of my entire soul, like these sweet mornings of spring which I enjoy with my whole heart.</p>
+                <a href="javascript:;" class="info">Play It</a>
+              </div>
+            </div>
+          </div>
+        </section>
+      </div>
+    </div>
+    <what-new />
+  </div>
+</template>
+<script>
+import WhatNew from '@/components/WhatNew'
+export default {
+  components: {
+    WhatNew
+  }
+}
+</script>
+<style lang="stylus" scoped>
+.games-contain
+  display flex
+  justify-content space-around
+.main
+  flex 1
+  display flex
+  justify-content space-around
+  flex-wrap wrap
+.side-bar
+  width 15%
+  float left
+  padding 10px 0
+  background aliceblue
+.side-bar h4
+  font-size 1.1em
+  text-transform uppercase
+  font-weight 500
+  text-align center
+  margin 0 0 1em
+ul.game-list
+  padding 0
+  margin 0.5em 0
+ul.game-list li
+  display block
+ul.game-list li a
+  font-size 0.875em
+  color #3c3a3e
+  font-weight 100
+  padding 0.2em 1em 0.5em
+  border-bottom 1px solid #3c3a3e
+  display block
+  text-transform uppercase
+ul.game-list li a:hover
+  color #ff7105
+.view
+  width 300px
+  height 200px
+  border 10px solid #fff
+  margin-bottom 10px
+  overflow hidden
+  position relative
+  text-align center
+  -webkit-box-shadow 1px 1px 2px #e6e6e6
+  -moz-box-shadow 1px 1px 2px #e6e6e6
+  box-shadow 1px 1px 2px #e6e6e6
+  cursor default
+  background #fff no-repeat center center
+.view .mask, .view .content
+  width 300px
+  height 200px
+  position absolute
+  overflow hidden
+  top 0
+  left 0
+.view img
+  display block
+  position relative
+.view h2
+  text-transform uppercase
+  color #fff
+  text-align center
+  position relative
+  font-size 17px
+  padding 10px
+  background rgba(0, 0, 0, 0.8)
+  margin 20px 0 0 0
+.view p
+  font-family Georgia, serif
+  font-style italic
+  font-size 12px
+  position relative
+  color #fff
+  padding 10px 20px 20px
+  text-align center
+.view .info
+  display inline-block
+  text-decoration none
+  padding 7px 14px
+  background #000
+  color #fff
+  text-transform uppercase
+  -webkit-box-shadow 0 0 1px #000
+  -moz-box-shadow 0 0 1px #000
+  box-shadow 0 0 1px #000
+.view .info:hover
+  -webkit-box-shadow 0 0 5px #000
+  -moz-box-shadow 0 0 5px #000
+  box-shadow 0 0 5px #000
+.view-first img
+  -webkit-transition all 0.2s linear
+  -moz-transition all 0.2s linear
+  -o-transition all 0.2s linear
+  -ms-transition all 0.2s linear
+  transition all 0.2s linear
+.view-first .mask
+  opacity 0
+  background-color rgba(219, 127, 8, 0.7)
+  -webkit-transition all 0.4s ease-in-out
+  -moz-transition all 0.4s ease-in-out
+  -o-transition all 0.4s ease-in-out
+  -ms-transition all 0.4s ease-in-out
+  transition all 0.4s ease-in-out
+.view-first h2
+  -webkit-transform translateY(-100px)
+  -moz-transform translateY(-100px)
+  -o-transform translateY(-100px)
+  -ms-transform translateY(-100px)
+  transform translateY(-100px)
+  opacity 0
+  -webkit-transition all 0.2s ease-in-out
+  -moz-transition all 0.2s ease-in-out
+  -o-transition all 0.2s ease-in-out
+  -ms-transition all 0.2s ease-in-out
+  transition all 0.2s ease-in-out
+.view-first p
+  -webkit-transform translateY(100px)
+  -moz-transform translateY(100px)
+  -o-transform translateY(100px)
+  -ms-transform translateY(100px)
+  transform translateY(100px)
+  opacity 0
+  -webkit-transition all 0.2s linear
+  -moz-transition all 0.2s linear
+  -o-transition all 0.2s linear
+  -ms-transition all 0.2s linear
+  transition all 0.2s linear
+.view-first:hover img
+  -webkit-transform scale(1.1, 1.1)
+  -moz-transform scale(1.1, 1.1)
+  -o-transform scale(1.1, 1.1)
+  -ms-transform scale(1.1, 1.1)
+  transform scale(1.1, 1.1)
+.view-first .info
+  opacity 0
+  -webkit-transition all 0.2s ease-in-out
+  -moz-transition all 0.2s ease-in-out
+  -o-transition all 0.2s ease-in-out
+  -ms-transition all 0.2s ease-in-out
+  transition all 0.2s ease-in-out
+.view-first:hover .mask
+  opacity 1
+.view-first:hover h2, .view-first:hover p, .view-first:hover .info
+  opacity 1
+  -webkit-transform translateY(0px)
+  -moz-transform translateY(0px)
+  -o-transform translateY(0px)
+  -ms-transform translateY(0px)
+  transform translateY(0px)
+.view-first:hover p
+  -webkit-transition-delay 0.1s
+  -moz-transition-delay 0.1s
+  -o-transition-delay 0.1s
+  -ms-transition-delay 0.1s
+  transition-delay 0.1s
+.view-first:hover .info
+  -webkit-transition-delay 0.2s
+  -moz-transition-delay 0.2s
+  -o-transition-delay 0.2s
+  -ms-transition-delay 0.2s
+  transition-delay 0.2s
+</style>
