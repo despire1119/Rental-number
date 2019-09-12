8d0da1b3698b3ea87d56b8df393c97a3220b903c
王皞然 <wanghaoran0218@hotmail.com>
2019-09-12 16:02:17 +0800

    tomerge

--- src/App.vue
+++ src/App.vue
@@ -12,6 +12,9 @@ export default {
   },
   created() {
     // 我添加一行注释你敢放进去嘛
+    // 我再添加一行注释
+    // 然后我改回来
+    // github预览
     if (sessionStorage.redirect) {
       const redirect = sessionStorage.redirect
       delete sessionStorage.redirect
