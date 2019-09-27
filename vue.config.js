245b01126c4275186d9fdf993de1bdc0e9f0cfab
王皞然 <wanghaoran0218@hotmail.com>
2019-09-27 09:31:59 +0800

    publish

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,6 @@
 module.exports = {
   runtimeCompiler: true,
-  publicPath: process.env.NODE_ENV === 'production' ? '/Rental-number/' : './',
+  // publicPath: process.env.NODE_ENV === 'production' ? '/Rental-number/' : './',
+  publicPath: './',
   lintOnSave: true
 }
