de6e111752d904201a72aef3c008ef4374ff6b82
王皞然 <wanghaoran0218@hotmail.com>
2019-10-16 09:15:11 +0800

    s

--- src/App.vue
+++ src/App.vue
@@ -11,9 +11,6 @@ export default {
     layout
   },
   created() {
-    // 我添加一行注释你敢放进去嘛
-    // 我再添加一行注释
-    // 然后我改回来
     // github预览
     if (sessionStorage.redirect) {
       const redirect = sessionStorage.redirect
